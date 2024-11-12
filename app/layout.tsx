2f635e1e1112b12cb4df57253b54765d5bd2927c
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-12 05:03:12 -0500

    style: add Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Chakra_Petch } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Chakra_Petch({ subsets: ["latin"], weight: "500" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background antialiased",
+          inter.className
+        )}
       >
         <ThemeProvider
           attribute="class"
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div>
-        Holi
-      </div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-8">
+        TinderYT
+      </h1>
       <ModeToggle />
     </main>
   );
