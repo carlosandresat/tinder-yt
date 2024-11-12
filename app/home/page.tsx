ecf46daaa65014d4d8dc325e311b6ede0d36a969
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-12 05:30:38 -0500

    feat: add navigation buttons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,17 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Page() {
     return (
-      <div className="flex h-screen w-full items-center justify-center px-4">
-        This is home
-      </div>
+      <main className="flex min-h-screen flex-col items-center">
+      <div className="w-full max-w-screen-xl flex items-center justify-between mt-8 px-8"><ModeToggle /><Button asChild>
+        <Link href="/">Salir</Link>
+      </Button></div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-8">
+        This is Home
+      </h1>
+      
+    </main>
     )
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
+      <div className="w-full max-w-screen-xl flex mt-8 px-8"><ModeToggle /></div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-8">
         TinderYT
       </h1>
-      <ModeToggle />
+      <Button asChild className="mt-8">
+        <Link href="/home">Entrar</Link>
+      </Button>
     </main>
   );
 }
