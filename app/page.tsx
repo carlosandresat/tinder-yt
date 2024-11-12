e90313496fc8c78049f38ba3230b5d1aa35e2d0c
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-12 04:43:26 -0500

    feat: add theme provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
-    <div>
-      Holi
-    </div>
+    <main className="flex min-h-screen flex-col items-center">
+      <div>
+        Holi
+      </div>
+      <ModeToggle />
+    </main>
   );
 }
