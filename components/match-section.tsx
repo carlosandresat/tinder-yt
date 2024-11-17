e164c49e2e3a09c482ba057e1075c7c7531d5326
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-17 00:50:19 -0500

    feat: add matches cards to MatchSection

--- components/match-section.tsx
+++ components/match-section.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 export function MatchSection() {
   return (
@@ -17,27 +18,68 @@ export function MatchSection() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 w-full">
         <Card>
           <CardHeader>
+            <CardTitle>#1</CardTitle>
+            <CardDescription>89% compatibilidad</CardDescription>
+          </CardHeader>
+          <CardContent>
             <div className="flex items-center gap-4">
               <Avatar className="w-20 h-20">
                 <AvatarImage src="/logo.webp" alt="Profile picture preview" />
                 <AvatarFallback>Foto</AvatarFallback>
               </Avatar>
 
-              <CardTitle className="scroll-m-20 text-xl font-semibold tracking-tight">
-                Nombre de la persona
-              </CardTitle>
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Carlos Arévalo
+              </h4>
             </div>
-            <CardDescription className="mt-8">
-              Descripción del perfil bla bla bla bla bla ewufhwe fewipu fhuwep
-              hfpew pf eup Soy una persona túnica y detergente. AAaaaaa no sé
-              que más poner. Papas fritas.
-            </CardDescription>
+          </CardContent>
+          <CardFooter className="justify-between">
+            <Button variant="secondary">Desbloquear</Button>
+            <Button>Ver respuestas</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>#2</CardTitle>
+            <CardDescription>85% compatibilidad</CardDescription>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <div className="flex items-center gap-4">
+              <Avatar className="w-20 h-20">
+                <AvatarImage src="/logo.webp" alt="Profile picture preview" />
+                <AvatarFallback>Foto</AvatarFallback>
+              </Avatar>
+
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Carlos Arévalo
+              </h4>
+            </div>
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
+          <CardFooter className="justify-between">
+            <Button variant="secondary">Desbloquear</Button>
+            <Button>Ver respuestas</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>#3</CardTitle>
+            <CardDescription>78% compatibilidad</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center gap-4">
+              <Avatar className="w-20 h-20">
+                <AvatarImage src="/logo.webp" alt="Profile picture preview" />
+                <AvatarFallback>Foto</AvatarFallback>
+              </Avatar>
+
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Carlos Arévalo
+              </h4>
+            </div>
+          </CardContent>
+          <CardFooter className="justify-between">
+            <Button variant="secondary">Desbloquear</Button>
+            <Button>Ver respuestas</Button>
           </CardFooter>
         </Card>
       </div>
