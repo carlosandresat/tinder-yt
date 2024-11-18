ebfac745fb83a62ac10bc092e2bca4467efb8e36
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-18 12:25:22 -0500

    feat: add schemas folder; add Register and Login schema

---
+++ schemas/index.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export const RegisterSchema = z.object({
+  fullname: z
+    .string()
+    .min(2, "Tu nombre debe contener más de 2 caracteres")
+    .max(50, "Tu nombre debe contener menos de 50 caracteres"),
+  sex: z.enum(["m", "f"]),
+  email: z
+    .string()
+    .email()
+    .endsWith(
+      "@yachaytech.edu.ec",
+      "Debes usar tu correo institucional para comprobar que eres estudiante de la poderosísima Yachay Tech 💅"
+    ),
+  password: z
+    .string()
+    .min(6, "Tu contraseña debe contener mínimo 6 caracteres")
+    .max(20, "Tu contraseña no debe poseer más de 20 caracteres"),
+});
+
+export const LoginSchema = z.object({
+    email: z
+      .string({
+        required_error: "Por favor ingresa tu correo institucional.",
+      })
+      .email("Correo inválido, revisa la sintaxis.")
+      .endsWith("@yachaytech.edu.ec", {
+        message: "Usa tu correo institucional",
+      }),
+    password: z.string().min(1, {
+      message: "Debes ingresar tu contraseña",
+    }),
+  });
