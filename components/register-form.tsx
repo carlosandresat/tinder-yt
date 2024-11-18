dd327f404013c73b9d6ea46a6e0a1aa066f6b059
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-18 15:06:39 -0500

    feat: complete Form integration to RegisterForm

--- components/register-form.tsx
+++ components/register-form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { z } from "zod";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,12 +10,54 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { RegisterSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useTransition } from "react";
+import { toast } from "@/hooks/use-toast";
 
 export function RegisterForm() {
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      fullname: "",
+      sex: undefined,
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof RegisterSchema>) {
+    startTransition(async () => {
+      try {
+        toast({
+          title: "You submitted the following values:",
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">
+                {JSON.stringify(data, null, 2)}
+              </code>
+            </pre>
+          ),
+        });
+      } catch (e) {}
+    });
+  }
+
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -20,54 +65,110 @@ export function RegisterForm() {
         <CardDescription>Llena tus datos para ingresar</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="name">Nombre</Label>
-            <Input id="name" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="sex">Sexo</Label>
-            <ToggleGroup type="single" id="sex" variant="outline">
-              <ToggleGroupItem value="m">Masculino</ToggleGroupItem>
-              <ToggleGroupItem value="f">Femenino</ToggleGroupItem>
-            </ToggleGroup>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="nombre.apellido@yachaytech.edu.ec"
-              required
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="fullname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombres</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Este será el nombre que verán tus matchs
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Contraseña</Label>
+            <FormField
+              control={form.control}
+              name="sex"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Sexo</FormLabel>
+                  <FormControl>
+                    <ToggleGroup
+                      type="single"
+                      variant="outline"
+                      value={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <ToggleGroupItem value="m">Masculino</ToggleGroupItem>
+                      <ToggleGroupItem value="f">Femenino</ToggleGroupItem>
+                    </ToggleGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>Usa tu correo institucional</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contraseña</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="repeatPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirmar Contraseña</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <p className="text-sm text-muted-foreground">
+              Una vez creada tu cuenta tendrás que esperar el correo con tu
+              código de verificación en las próximas horas. Enviaré los correos
+              manualmente así que paciencia 😅
+            </p>
+            <div className="w-full flex justify-center">
+              <Button
+                type="submit"
+                disabled={isPending}
+                className="w-full sm:w-auto "
+              >
+                Crea tu cuenta
+              </Button>
             </div>
-            <Input id="password" type="password" required />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Confirma tu contraseña</Label>
+            <div className="mt-4 text-center text-sm">
+              ¿Ya tienes cuenta?{" "}
+              <Link href="/auth/login" className="underline">
+                Entra aquí
+              </Link>
             </div>
-            <Input id="password" type="password" required />
-          </div>
-          <p className="text-sm text-muted-foreground">
-            Una vez creada tu cuenta tendrás que esperar el correo con tu código
-            de verificación en las próximas horas. Enviaré los correos
-            manualmente así que paciencia 😅
-          </p>
-          <Button type="submit" className="w-full">
-            Crea tu cuenta
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          ¿Ya tienes cuenta?{" "}
-          <Link href="/auth/login" className="underline">
-            Entra aquí
-          </Link>
-        </div>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
