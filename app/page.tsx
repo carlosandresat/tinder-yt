7233c804dd039084243c84117fe99d3990221fd4
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-17 21:54:06 -0500

    feat: add logo to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="w-full max-w-screen-xl flex mt-8 px-8"><ModeToggle /></div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full max-w-screen-xl flex mt-8 px-8 absolute top-0"><ModeToggle /></div>
+      <div className="w-40 h-40 rounded-full">
+      <Image src="/logo.webp" width={800} height={800} alt="TinderYT Logo" className="rounded-full"></Image>
+      </div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-8">
         TinderYT
       </h1>
