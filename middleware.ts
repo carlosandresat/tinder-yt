e52cca1ca8af7f8aa612aaab42d99fa3b5ea9726
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-28 12:22:30 -0500

    feat: add middleware file with initial configuration

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  // req.auth
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
