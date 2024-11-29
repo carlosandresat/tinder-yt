ee0dc0ff4a217d50bcb301a7a03c7be498899fad
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-29 00:00:02 -0500

    feat: define publicRoutes, apiRoutes for middleware redirections

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,32 @@
 import authConfig from "@/auth.config"
 import NextAuth from "next-auth"
-import { NextRequest } from "next/server";
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes} from "@/routes";
 
 
 const { auth } = NextAuth(authConfig)
-export default auth(async function middleware(req: NextRequest) {
-  // Your custom middleware logic goes here
-})
+export default auth(async function middleware(req) {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+  return;
+});
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
