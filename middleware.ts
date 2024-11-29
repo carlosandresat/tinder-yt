2e0fa7786988fc37e2ca04aa98b615a7b787de11
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-28 21:59:45 -0500

    feat: extend auth configuration for edge compatibility

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import { auth } from "@/auth";
+import authConfig from "@/auth.config"
+import NextAuth from "next-auth"
+import { NextRequest } from "next/server";
 
-export default auth((req) => {
-  // req.auth
-});
+
+const { auth } = NextAuth(authConfig)
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
+})
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
