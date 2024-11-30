cdf90a64283c39f3b347f02db6997f0d578cd748
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-30 17:12:51 -0500

    feat: extend session to have access to the user id

--- auth.ts
+++ auth.ts
@@ -6,5 +6,19 @@ import authConfig from "@/auth.config"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: {strategy: "jwt"},
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if(token.sub && session.user){
+        session.user.id = token.sub
+      }
+      return session
+    },
+  },
   ...authConfig,
 })
