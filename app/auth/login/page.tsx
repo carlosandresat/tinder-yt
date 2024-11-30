15f7b5232bf7ef95435e72f8ce302b5819fe6d59
Carlos Arevalo <carlosandresat@hotmail.com>
2024-11-29 21:09:03 -0500

    feat: add return button to register and login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,20 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { LoginForm } from "@/components/login-form";
+import { ArrowBigLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="w-full max-w-screen-xl flex mt-8 px-8 md:absolute"><ModeToggle /></div>
-      <div className="my-auto px-8 w-full">
+    <main className="flex min-h-screen flex-col justify-center">
+      <div className="w-full flex items-center justify-between mt-8 px-8 absolute top-0">
+        <Link href="/">
+          <ArrowBigLeft />
+        </Link>
+        <ModeToggle />
+      </div>
+      <div className="px-8">
         <LoginForm />
       </div>
     </main>
-  )
+  );
 }
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,13 +1,20 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { RegisterForm } from "@/components/register-form";
+import { ArrowBigLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="w-full max-w-screen-xl flex mt-8 px-8 md:absolute"><ModeToggle /></div>
-      <div className="p-8">
+    <main className="flex min-h-screen flex-col justify-center">
+      <div className="w-full flex items-center justify-between p-8">
+        <Link href="/">
+          <ArrowBigLeft />
+        </Link>
+        <ModeToggle />
+      </div>
+      <div className="px-8 pb-8">
         <RegisterForm />
       </div>
     </main>
-  )
+  );
 }
