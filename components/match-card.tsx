6a365e8b3748dc589d6f1aed3a724fba4e10ddda
Carlos Arevalo <carlosandresat@hotmail.com>
2025-04-02 23:52:02 -0500

    feat: implement MatchCard component and integrate it into MatchSection for improved match display

---
+++ components/match-card.tsx
@@ -0,0 +1,92 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { LockKeyhole, VenetianMask } from "lucide-react";
+import { PaymentDialog } from "@/components/payment-dialog";
+import { MatchAnswersDialog } from "@/components/match-answers-dialog";
+
+interface MatchData {
+  tier: number;
+  unlocked: boolean;
+  score: number;
+  image: string | null;
+  name: string | null;
+  description: string | null;
+  contact: string | null;
+  answers: {
+    question1: number;
+    question2: string[];
+    question3: number;
+  };
+}
+
+export function MatchCard({
+  tier,
+  unlocked,
+  score,
+  image,
+  name,
+  description,
+  contact,
+  answers,
+}: MatchData) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader>
+        <CardTitle>#{tier}</CardTitle>
+        <CardDescription>Compatibilidad: {score}/1000</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex items-center gap-4">
+          {unlocked && image ? (
+            <div className="w-20 h-20">
+              <Image
+                src={image}
+                alt="Profile picture preview"
+                className="rounded-full h-full w-full object-cover"
+                width={500}
+                height={500}
+              />
+            </div>
+          ) : (
+            <div className="w-20 h-20 flex items-center justify-center bg-secondary rounded-full">
+              <VenetianMask className="w-10 h-10" />
+            </div>
+          )}
+
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            {unlocked ? name : `Match #${tier}`}
+          </h4>
+        </div>
+      </CardContent>
+      <CardFooter className={unlocked ? "justify-end" : "justify-between"}>
+        {!unlocked ? <PaymentDialog /> : null}
+        <MatchAnswersDialog />
+      </CardFooter>
+      {unlocked ? (
+        <div className="flex flex-col p-6 border-t">
+          <p className="leading-normal mt-2">{description}</p>
+          <h4 className="scroll-m-20 text-lg font-semibold tracking-tight mt-2">
+            Redes sociales
+          </h4>
+          <p className="leading-normal">{contact}</p>
+        </div>
+      ) : (
+        <div className="flex flex-col p-6 border-t justify-center items-center flex-grow space-y-2">
+          <LockKeyhole />
+          <p className="text-sm text-muted-foreground text-center">
+            Desbloquea este match para conocer su nombre, descripción y redes
+            sociales
+          </p>
+        </div>
+      )}
+    </Card>
+  );
+}
--- components/match-section.tsx
+++ components/match-section.tsx
@@ -11,99 +11,69 @@ import Image from "next/image";
 import { VenetianMask, LockKeyhole } from "lucide-react";
 import { PaymentDialog } from "@/components/payment-dialog";
 import { MatchAnswersDialog } from "@/components/match-answers-dialog";
+import { MatchCard } from "@/components/match-card";
 
 export function MatchSection() {
+  const matchsData = [
+    {
+      unlocked: true,
+      score: 975,
+      image: "/me.jpg",
+      name: "Carlos Arévalo",
+      description: "This is a generic description lol equisde ajio ajio ajio, a veces quisiera comer tartas con limón.",
+      contact: "Instagram: @carlosandresat",
+      answers: {
+        question1: 2,
+        question2: ["1", "2", "5"],
+        question3: 3
+      }
+    },
+    {
+      unlocked: false,
+      score: 845,
+      image: null,
+      name: null,
+      description: null,
+      contact: null,
+      answers: {
+        question1: 1,
+        question2: ["3", "4", "5"],
+        question3: 5
+      }
+    },
+    {
+      unlocked: false,
+      score: 759,
+      image: null,
+      name: null,
+      description: null,
+      contact: null,
+      answers: {
+        question1: 1,
+        question2: ["1", "2", "4"],
+        question3: 1
+      }
+    }
+  ]
   return (
     <div className="flex flex-col w-full">
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight self-center">
         Tus Matchs:
       </h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 w-full">
-        <Card>
-          <CardHeader>
-            <CardTitle>#1</CardTitle>
-            <CardDescription>89% compatible</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center gap-4">
-              <div className="w-20 h-20">
-                <Image
-                  src="/me.jpg"
-                  alt="Profile picture preview"
-                  className="rounded-full h-full w-full object-cover"
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                Carlos Arévalo
-              </h4>
-            </div>
-          </CardContent>
-          <CardFooter className="justify-end">
-            <MatchAnswersDialog />
-          </CardFooter>
-          <div className="flex flex-col p-6 border-t">
-            <p className="leading-normal mt-2">
-              This is a generic description lol equisde ajio ajio ajio, a veces
-              quisiera comer tartas con limón.
-            </p>
-            <h4 className="scroll-m-20 text-lg font-semibold tracking-tight mt-2">
-              Redes sociales
-            </h4>
-            <p className="leading-normal">Intagram: @carlosandresat</p>
-          </div>
-        </Card>
-        <Card className="flex flex-col">
-          <CardHeader>
-            <CardTitle>#2</CardTitle>
-            <CardDescription>85% compatible</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center gap-4">
-              <div className="w-20 h-20 flex items-center justify-center bg-secondary rounded-full">
-                <VenetianMask className="w-10 h-10" />
-              </div>
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                Match #2
-              </h4>
-            </div>
-          </CardContent>
-          <CardFooter className="justify-between">
-            <PaymentDialog />
-            <Button>Ver respuestas</Button>
-          </CardFooter>
-          <div className="flex flex-col p-6 border-t justify-center items-center flex-grow space-y-2">
-            <LockKeyhole />
-            <p className="text-sm text-muted-foreground text-center">Desbloquea este match para conocer su nombre, descripción y redes sociales</p>
-          </div>
-
-        </Card>
-        <Card className="flex flex-col">
-          <CardHeader>
-            <CardTitle>#3</CardTitle>
-            <CardDescription>78% compatible</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center gap-4">
-              <div className="w-20 h-20 flex items-center justify-center bg-secondary rounded-full">
-                <VenetianMask className="w-10 h-10" />
-              </div>
-
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                Match #3
-              </h4>
-            </div>
-          </CardContent>
-          <CardFooter className="justify-between">
-            <PaymentDialog />
-            <Button>Ver respuestas</Button>
-          </CardFooter>
-          <div className="flex flex-col p-6 border-t justify-center items-center flex-grow space-y-2">
-            <LockKeyhole />
-            <p className="text-sm text-muted-foreground text-center">Desbloquea este match para conocer su nombre, descripción y redes sociales</p>
-          </div>
-        </Card>
+        {matchsData.map((match, index) => (
+          <MatchCard
+            key={index}
+            tier={index + 1}
+            unlocked={match.unlocked}
+            score={match.score}
+            image={match.image}
+            name={match.name}
+            description={match.description}
+            contact={match.contact}
+            answers={match.answers}
+          />
+        ))}
       </div>
     </div>
   );
