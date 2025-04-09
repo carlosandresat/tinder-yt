2353da26213caaf955d8bb471b59de22c0b23034
Carlos Arevalo <carlosandresat@hotmail.com>
2025-04-08 23:24:57 -0500

    feat: display available unlocks in MatchSection component

--- components/match-section.tsx
+++ components/match-section.tsx
@@ -1,12 +1,14 @@
-import { getTopMatches } from "@/actions/match-data";
+import { getAvailableUnlocks, getTopMatches } from "@/actions/match-data";
 import { MatchCard } from "@/components/match-card";
 
 export async function MatchSection() {
   const matches = await getTopMatches();
+  const availableUnlocks = await getAvailableUnlocks();
 
   return (
     <div className="flex flex-col w-full">
-      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight self-center">
+      <div className="text-lg font-semibold self-end">Desbloqueos disponibles: {availableUnlocks}</div>
+      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight self-center mt-6">
         Tus Matchs
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 w-full">
