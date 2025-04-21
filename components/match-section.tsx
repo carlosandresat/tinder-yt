02d3f2d3ae983b6d5417bc709ec61fd83de7c82a
Carlos Arevalo <carlosandresat@hotmail.com>
2025-04-21 00:39:16 -0500

    feat: add countdown timer with badge for available unlocks in MatchSection

--- components/match-section.tsx
+++ components/match-section.tsx
@@ -2,18 +2,53 @@ import { getAvailableUnlocks, getTopMatches } from "@/actions/match-data";
 import { MatchCard } from "@/components/match-card";
 import { PaymentDialog } from "@/components/payment-dialog";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 export async function MatchSection() {
   const matches = await getTopMatches();
   const availableUnlocks = await getAvailableUnlocks();
   const hasAvailableUnlocks = availableUnlocks > 0;
+  const timeLeft = new Date("2025-04-21T17:00").getTime() - new Date().getTime();
 
   return (
     <div className="flex flex-col w-full max-w-screen-xl p-8">
-      <div className="text-lg font-semibold self-end">Desbloqueos disponibles: {availableUnlocks}</div>
-      <PaymentDialog>
-        <Button variant="secondary" className="self-end mt-2">¿Quieres más desbloqueos?</Button>
-      </PaymentDialog>
+      <div className="text-lg font-semibold self-end">
+        Desbloqueos disponibles: {availableUnlocks}
+      </div>
+      <div className=" flex flex-col md:flex-row space-x-4 self-end mt-2 space-y-4 md:space-y-0">
+        {
+          //Time left with discount in hours and minutes with a countdown
+          timeLeft > 0 ? (
+            <div className="flex flex-col md:flex-row items-end md:items-center justify-center md:space-x-2">
+              <h2 className="font-semibold text-center">
+                ¡Descuento del 50% en desbloqueos! Quedan
+              </h2>
+              <Badge className="space-x-4">
+                <p
+                  aria-live="polite"
+                  suppressHydrationWarning
+                >
+                  {Math.floor(timeLeft / (1000 * 60 * 60))} horas
+                </p>
+                <p
+                  aria-live="polite"
+                  suppressHydrationWarning
+                >
+                  {Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))}{" "}
+                  minutos
+                </p>
+              </Badge>
+            </div>
+          ) : (
+            <p className="text-sm text-muted-foreground">
+              ¡El descuento ha terminado!
+            </p>
+          )
+        }
+        <PaymentDialog>
+          <Button variant="secondary">¿Quieres más desbloqueos?</Button>
+        </PaymentDialog>
+      </div>
       <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight self-center mt-6">
         Tus Matchs
       </h1>
