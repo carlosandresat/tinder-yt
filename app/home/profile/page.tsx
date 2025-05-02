7f6ee6ef984e467e5f0280bdc30f6134f26b0587
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-02 16:37:09 -0500

    feat: add user profile page component

---
+++ app/home/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold tracking-tight text-center">
+        Perfil de usuario
+      </h1>
+
+    </main>
+  );
+}
