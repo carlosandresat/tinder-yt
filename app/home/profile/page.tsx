a3f6899ab2ba35b447f09cabe26d44b474963ea6
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-02 22:45:57 -0500

    feat: enhance user profile page with authentication check and user image display with name and email

--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -1,10 +1,35 @@
-export default function Page() {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function Page() {
+  const session = await auth();
+  if (!session?.user) {
+    return (
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-semibold tracking-tight text-center">
+          No has iniciado sesión
+        </h1>
+      </main>
+    );
+  }
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-semibold tracking-tight text-center">
         Perfil de usuario
       </h1>
-
+      <div className="w-32 h-32 rounded-full mt-8">
+        <Image
+          src={session.user.image ?? ""}
+          alt="User Avatar"
+          className="rounded-full w-full h-full object-cover"
+          width={128}
+          height={128}
+        />
+      </div>
+      <h2 className="text-xl font-semibold tracking-tight mt-4">
+        {session.user.name}
+      </h2>
+      <p className="text-lg text-muted-foreground mt-2">{session.user.email}</p>
     </main>
   );
 }
