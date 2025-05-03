4be5628ef145ed0aa098b05eadefe62505cd04a6
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-02 21:55:55 -0500

    feat: create settings page component

---
+++ app/home/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-semibold tracking-tight text-center">
+        Configuración
+      </h1>
+    </main>
+  );
+}
