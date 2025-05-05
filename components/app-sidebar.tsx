c2e90ba3687e8905ba6e2339b73503a1a1e63bef
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-04 22:35:28 -0500

    feat: implement sidebar menu with navigation items and logo

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,9 +1,83 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import { Home, Settings, CircleUserRound, LockKeyhole } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
+import { LogoutButton } from "@/components/logout-button";
+
+const items = [
+  {
+    title: "Inicio",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Perfil",
+    url: "/home/profile",
+    icon: CircleUserRound,
+  },
+  {
+    title: "Lista de Bloqueados",
+    url: "/home/blocklist",
+    icon: LockKeyhole,
+  },
+  {
+    title: "Configuración",
+    url: "/home/settings",
+    icon: Settings,
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarHeader className="flex-row items-center justify-center gap-2 px-4 py-4 border-b font-semibold">
+        <div className="flex items-center justify-center">
+          <Image
+            className="size-12 rounded-full"
+            src="/logo.webp"
+            alt="Logo"
+            width={80}
+            height={80}
+          />
+        </div>
+        TinderYT
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menú</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className="flex-row items-center justify-between">
+        <LogoutButton />
+        <ModeToggle />
+      </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
