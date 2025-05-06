48913cd95e712c33107e8de156596c0c87452635
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-05 23:47:35 -0500

    feat: add input fields for user description, WhatsApp, Instagram, and Facebook in profile page

--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -1,4 +1,7 @@
 import { auth } from "@/auth";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default async function Page() {
@@ -28,6 +31,50 @@ export default async function Page() {
         {session.user.name}
       </h2>
       <p className="text-lg text-muted-foreground mt-2">{session.user.email}</p>
+
+      <div className="flex flex-col space-y-8 sm:max-w-2xl w-full px-8 mt-8 pb-28">
+        <div className="space-y-2">
+          <Label htmlFor="description" className="text-base">
+            Descripción
+          </Label>
+          <Textarea
+            id="description"
+            placeholder="Escribe algo sobre ti..."
+            className="resize-none h-32 w-full "
+            defaultValue="Descripción de ejemplo"
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="whatsapp" className="text-base">
+            Whatsapp
+          </Label>
+          <Input
+            id="whatsapp"
+            placeholder="+593999999999"
+            defaultValue="+593987654321"
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="instagram" className="text-base">
+            Instagram
+          </Label>
+          <Input
+            id="instagram"
+            placeholder="@tu_instagram"
+            defaultValue="@ejemplo_instagram"
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="facebook" className="text-base">
+            Facebook
+          </Label>
+          <Input
+            id="facebook"
+            placeholder="Tu Facebook"
+            defaultValue="https://www.facebook.com/ejemplo"
+          />
+        </div>
+      </div>
     </>
   );
 }
