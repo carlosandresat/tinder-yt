d7bbfdf9569c15192ba048114495f4dd770ec1b0
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-07 17:29:37 -0500

    feat: add birthday cake image to AddBirthdayDialog for enhanced visual appeal

--- components/add-birthday-dialog.tsx
+++ components/add-birthday-dialog.tsx
@@ -9,6 +9,7 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { AddBirthdayForm } from "@/components/add-birthday-form";
+import Image from "next/image";
 
 export function AddBirthdayDialog() {
   return (
@@ -23,6 +24,13 @@ export function AddBirthdayDialog() {
             Para verificar tu edad, por favor añade tu fecha de nacimiento. Esta
             información no será compartida con otros usuarios.
           </DialogDescription>
+          <Image
+            src="/birthday-cake.png"
+            alt="Birthday cake"
+            width={100}
+            height={100}
+            className="mx-auto"
+          />
         </DialogHeader>
 
         <AddBirthdayForm />
