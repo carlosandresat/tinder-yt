73697217d3c996a71f8427b81b819cbc72f763a5
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-20 10:21:53 -0500

    feat: restructure settings page layout for improved organization

--- app/home/settings/page.tsx
+++ app/home/settings/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Slider } from "@/components/ui/slider";
 import { Switch } from "@/components/ui/switch";
@@ -9,30 +10,30 @@ export default function Page() {
       <h1 className="text-2xl font-semibold tracking-tight text-center">
         Configuración
       </h1>
-      <div className="space-y-2 pt-6 px-8 w-full sm:max-w-2xl">
-        <Label>Matchear con</Label>
-        <ToggleGroup type="single" variant="outline" className="flex-wrap">
-          <ToggleGroupItem value="m">Hombres</ToggleGroupItem>
-          <ToggleGroupItem value="f">Mujeres</ToggleGroupItem>
-          <ToggleGroupItem value="both">Ambos</ToggleGroupItem>
-        </ToggleGroup>
-      </div>
-      <div className="space-y-2 pt-6 px-8 w-full sm:max-w-2xl">
-        <Label>Rango de edad</Label>
-        <Slider
-          defaultValue={[18, 99]}
-          max={99}
-          min={18}
-          step={1}
-          className="w-full"
-        />
-        <div className="flex justify-between text-sm text-muted-foreground">
-          <p>18</p>
-          <p>99</p>
+      <div className="flex flex-col space-y-8 pt-8 px-8 w-full sm:max-w-2xl">
+        <div className="space-y-2 w-full">
+          <Label>Matchear con</Label>
+          <ToggleGroup type="single" variant="outline" className="flex-wrap">
+            <ToggleGroupItem value="m">Hombres</ToggleGroupItem>
+            <ToggleGroupItem value="f">Mujeres</ToggleGroupItem>
+            <ToggleGroupItem value="both">Ambos</ToggleGroupItem>
+          </ToggleGroup>
         </div>
-      </div>
-      <div className="space-y-2 pt-6 px-8 w-full sm:max-w-2xl">
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm w-full sm:max-w-2xl space-x-4">
+        <div className="space-y-2 w-full">
+          <Label>Rango de edad</Label>
+          <Slider
+            defaultValue={[18, 99]}
+            max={99}
+            min={18}
+            step={1}
+            className="w-full"
+          />
+          <div className="flex justify-between text-sm text-muted-foreground">
+            <p>18</p>
+            <p>99</p>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm w-full space-x-4">
           <div className="space-y-0.5">
             <Label>Visible en TinderYT</Label>
             <p className="text-sm text-muted-foreground">
@@ -42,6 +43,7 @@ export default function Page() {
           </div>
           <Switch />
         </div>
+        <Button className="self-end">Guardar cambios</Button>
       </div>
     </>
   );
