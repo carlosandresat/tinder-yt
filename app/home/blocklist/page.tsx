d3f2d507910b71905d4dbc49faaddd1c58ebea2d
Carlos Arevalo <carlosandresat@hotmail.com>
2025-05-19 20:27:23 -0500

    style: adjust max-width for blocklist description and table layout

--- app/home/blocklist/page.tsx
+++ app/home/blocklist/page.tsx
@@ -17,13 +17,13 @@ export default function Page() {
       <h1 className="text-2xl font-semibold tracking-tight text-center">
         Lista de Bloqueados
       </h1>
-      <p className="text-muted-foreground max-w-screen-lg px-8 pt-2 text-center">
+      <p className="text-muted-foreground sm:max-w-2xl px-8 pt-2 text-center">
         ¿Tienes a alguien que no quieres ni ver? Agrega aquí a tu ex, a tus amix
         o a cualquiera que no quieras ver en TinderYT. No te preocupes, no se
         enterarán de que los bloqueaste.
       </p>
       <BlocklistForm />
-      <div className="flex justify-center max-w-screen-lg mt-8 px-8 w-full pb-28">
+      <div className="flex justify-center sm:max-w-2xl mt-8 px-8 w-full pb-28">
         <Table>
           <TableCaption>Lista de usuarios bloqueados.</TableCaption>
           <TableHeader>
